2cee7387366d36a67f05a9cb2b1791bb2591bc30
Wefrit <lopes.nathanlemes@gmail.com>
2026-07-23 15:07:55 -0300

    Altera filter_movie_list; Refatoração de add_favorite e remove_favorite; Criação de change_favorite no lugar de add e remove; Criação da função movie_selection; Criaçaõ de docstrings para as funções.

--- functions.py
+++ functions.py
@@ -4,7 +4,9 @@ import json
 
 
 # adicionar filme
-def add_movie(movie_list):
+def add_movie(movie_list: list):
+    '''Add a movie to the movie list'''
+
     while True:
         movie = input('Qual filme deseja adicionar na lista? ')
         if movie.strip() == '':
@@ -16,80 +18,60 @@ def add_movie(movie_list):
             break
 
 # mostrar listas de filmes
-def filter_movie_list(movie_list, mode='all'):
+def filter_movie_list(movie_list: list, mode:str | None = ''):
+    '''FIlters a list accordingly with the mode \n
+    modes can be:\n
+    'favorites' -> returns a list with movies having the key 'favorite' = True;\n
+    \n
+    'non_favorites' -> returns a list with movies having the key 'favorite' = False;\n
+    \n
+    None -> returns a list with all movies\n
+    '''
+    favorite_list = []
+    non_favorite_list=[]
+
+    for movie in movie_list:
+        if movie['favorite']:
+            favorite_list.append(movie)
+        else:
+            non_favorite_list.append(movie)
+
     if mode == 'favorites':
-        favorite_list = []
-        for movie in movie_list:
-            if movie['favorite']:
-                favorite_list.append(movie)
         return favorite_list
-    
     elif mode == 'non_favorites':
-        non_favorite_list=[]
-        for movie in movie_list:
-            if not movie['favorite']:
-                non_favorite_list.append(movie)
         return non_favorite_list
-    
     else:
         return movie_list
     
 # adicionar filme aos favoritos
-def add_favorite(movie_list):
-    if movie_list:
-        non_favorite_list = filter_movie_list(movie_list, mode='non_favorites')
-        if non_favorite_list:
-            print('\nLISTA DE FILMES\n')
-            exhibit_list_movies(non_favorite_list)
-            while True:
-                option = input('Selecione o filme que deseja favoritar(número) ou aperte 0 para cancelar: ')
-                if option.isdigit():
-                    if int(option) == 0:
-                        break
-                    elif int(option) > len(non_favorite_list):
-                        print('Selecione um valor válido.\n')
-                    else:
-                        movie = non_favorite_list[int(option)-1]
-                        movie['favorite'] = True
-                        save_movies(movie_list)
-                        print('Filme favoritado com sucesso.\n')
-                        break
-                else:
-                    print('Selecione um valor válido.\n')
-        else:
-            print('Todos os filmes ja foram favoritados.')
-    else:
-        exhibit_list_movies(movie_list)
-        
-# remover filme dos faovritos
-def remove_favorite(movie_list):
-    if movie_list:
-        favorite_list = filter_movie_list(movie_list, mode='favorites')
-        if favorite_list:
-            print('\nLISTA DE FILMES\n')
-            exhibit_list_movies(favorite_list)
-            while True:
-                option = input('Selecione o filme que deseja remover(número) ou aperte 0 para cancelar: ')
-                if option.isnumeric():
-                    if int(option) > len(favorite_list):
-                        print('Selecione um valor válido.\n')
-                    elif int(option) == 0 :
-                        break
-                    else:
-                        favorite_list[int(option)-1]['favorite'] = False
-                        save_movies(movie_list)
-                        print('Filme removido dos favoritos.\n')
-                        break
-                else:
-                    print('Selecione um valor válido.\n')
-        else:
-            exhibit_list_movies(favorite_list)
-    else:
-        exhibit_list_movies(movie_list)
+def add_favorite(movie_list:list):
+    '''Change the key 'favorite' of a movie from False to True'''
 
+    if movie_list:       
+        movie = movie_selection(movie_list)
+        if movie is not None:
+            movie['favorite'] = True
+            save_movies(movie_list)
+            print('Filme adicionado aos favoritos.\n')
+    else:
+        print('Não existem filmes nessa lista.')
+
+# remover filme dos faovritos
+def remove_favorite(movie_list:list):
+    '''Change the key 'favorite' of a movie from True to False'''
+
+    if movie_list:       
+        movie = movie_selection(movie_list)
+        if movie is not None:
+            movie['favorite'] = False
+            save_movies(movie_list)
+            print('Filme removido dos favoritos.\n')
+    else:
+        print('Não existem filmes nessa lista.')
 
 # mostrar listas de filmes
-def exhibit_list_movies(movie_list):
+def exhibit_list_movies(movie_list:list):
+    '''Prints a movie list in order'''
     if movie_list:
         n = 1
         for movie in movie_list:
@@ -100,16 +82,21 @@ def exhibit_list_movies(movie_list):
     else:
         print('Não existem filmes cadastrados nesta seção.\n')
 
-
 # limpar terminal
 def clean_screen():
+    '''Cleans the terminal'''
+
     os.system('cls')
 
 # esperar usuário ler mensagem 
 def wait_user():
+    '''Prints a input message'''
+
     input('Aperte Enter para retornar ao menu')
 
 def load_movies():
+    '''Returns a list if exists ou a empty list if it doesn't'''
+
     try:
         with open('movies_list.json', 'r') as file:
             return json.load(file)
@@ -117,6 +104,43 @@ def load_movies():
     except FileNotFoundError:
         return []
 
-def save_movies(movie_list):
+def save_movies(movie_list:list):
+    '''Alters a existing movies_list.json file or creates one if it doesn't exists '''
+
     with open('movies_list.json','w') as file:
         json.dump(movie_list, file)
+
+def movie_selection(movie_list:list):
+    '''Display a input message to the user to select a movie\n
+    Returns a movie based on the option selected
+    '''
+
+    while True:
+        option = input('Selecione um filme (número) ou aperte 0 para cancelar: ')
+        if option.isnumeric():
+            if int(option) > len(movie_list):
+                print('Selecione um valor válido.\n')
+            elif option == '0':
+                break
+            else:
+                return movie_list[int(option)-1]
+        else:
+            print('Selecione um valor válido.\n')
+
+
+def change_favorite(movie_list:list):
+    '''Alters the boolean of the key 'favorite'''
+
+    if movie_list:       
+            movie = movie_selection(movie_list)
+            if movie is not None:
+                if movie['favorite'] == True:
+                    movie['favorite'] = False
+                    print('Filme removido dos favoritos.\n')
+                else:
+                    movie['favorite'] = True
+                    print('Filme adicionado aos favoritos.\n')
+                save_movies(movie_list)
+    else:
+        print('Não existem filmes nessa lista.')
+    
--- main.py
+++ main.py
@@ -6,12 +6,12 @@ def menu(movie_list):
                  '3 - Adicionar Favoritos',
                  '4 - Filmes Favoritos',
                  '5 - Remover Favorito',
-                 '6 - Sair']
+                 '0 - Sair']
     while True:
         for option in MENU_LIST:
             print(option)
         selected_option = input('Selecione uma opção: ')
-        if selected_option in ('1','2','3','4','5','6'):
+        if selected_option in ('1','2','3','4','5','0'):
             if selected_option == '1':
                 add_movie(movie_list)
                 wait_user()
@@ -20,7 +20,10 @@ def menu(movie_list):
                 exhibit_list_movies(movie_list)
                 wait_user()
             elif selected_option == '3':
-                add_favorite(movie_list)
+                non_favorite_list = filter_movie_list(movie_list, mode='non_favorites')
+                print('\nLISTA DE FILMES\n')
+                exhibit_list_movies(non_favorite_list)
+                change_favorite(non_favorite_list)
                 wait_user()
             elif selected_option == '4':
                 favorite_list = filter_movie_list(movie_list, mode='favorites')
@@ -28,9 +31,12 @@ def menu(movie_list):
                 exhibit_list_movies(favorite_list)
                 wait_user()
             elif selected_option == '5':
-                remove_favorite(movie_list)
+                favorite_list = filter_movie_list(movie_list, mode='favorites')
+                print('\nFILMES FAVORITOS\n')
+                exhibit_list_movies(favorite_list)
+                change_favorite(favorite_list)
                 wait_user()
-            elif selected_option == '6':
+            elif selected_option == '0':
                 break
         else:
             print('Selecione uma opção válida.\n')
